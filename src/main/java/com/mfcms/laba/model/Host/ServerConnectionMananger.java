39a773a89bd9258eb626aea11ff3a316c2683d45
mfcms <mfcms@mail.ru>
2023-03-17 22:42:12 +0300

    server still in progress

--- src/main/java/com/mfcms/laba/model/Host/ServerConnectionMananger.java
+++ src/main/java/com/mfcms/laba/model/Host/ServerConnectionMananger.java
@@ -1,16 +1,28 @@
-package com.mfcms.laba.model.Host;
+// //package com.mfcms.laba.model.Host;
 
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+// //import java.io.IOException;
+// import java.io.ObjectOutputStream;
+// import java.io.OutputStream;
+// import java.net.Socket;
 
-import com.mfcms.laba.console.commands.Configuration.Config;
+// import com.mfcms.laba.console.commands.Configuration.Config;
 
-public class ServerConnectionMananger {
-    public Socket {
-    Socket socket = new Socket(Config.IP, port);
+// public class ServerConnectionMananger {
+//     public void Socket() {
+//     Socket socket;
+//     try {
+//         socket = new Socket(Config.IP, Config.PORT);
+//     } catch (IOException e) {
+//         e.printStackTrace();
+//     }
 
-    OutputStream os = socket.getOutputStream();
-    ObjectOutputStream oos = new ObjectOutputStream(os);
-    }
-}
+//     OutputStream os;
+//     try {
+//         socket = new Socket(Config.IP, Config.PORT);
+//         os = socket.getOutputStream()
+//     (ObjectOutputStream oos = new ObjectOutputStream(os)) {
+//     } catch (IOException e) {
+//         e.printStackTrace();
+//     }
+//     }
+// }
