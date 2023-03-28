52b82bc5b96d61fc72753579f76ec27c805223e8
mfcms <mfcms@mail.ru>
2023-03-28 22:36:52 +0300

    seeerver

--- src/main/java/com/mfcms/laba/client/ApplicationBase.java
+++ src/main/java/com/mfcms/laba/client/ApplicationBase.java
@@ -1,12 +1,13 @@
 package com.mfcms.laba.client;
 
+import com.mfcms.laba.client.console.CommandExecutor;
 import com.mfcms.laba.client.net.ClientConnectionManager;
 import com.mfcms.laba.client.parsing.FieldReader;
 import com.mfcms.laba.client.parsing.object.MusicBandReader;
 import com.mfcms.laba.common.IOManager;
-import com.mfcms.laba.common.console.CommandExecutor;
+import com.mfcms.laba.common.model.MusicBandGetter;
+import com.mfcms.laba.common.model.MusicBandManager;
 import com.mfcms.laba.server.CommandService;
-import com.mfcms.laba.server.MusicBandManager;
 import com.mfcms.laba.server.database.DatabaseService;
 
 public abstract class ApplicationBase {
@@ -17,7 +18,8 @@ public abstract class ApplicationBase {
     protected MusicBandReader musicBandReader;
     protected FieldReader fieldReader;
     protected DatabaseService databaseService;
-    protected ClientConnectionManager clientConnectionManager;
+    protected ClientConnectionManager ConnectionManager;
+    protected MusicBandGetter musicBandGetter;
 
     public void run() {
         System.out.println(this.getClass().getSimpleName() + " is running");
@@ -54,8 +56,12 @@ public abstract class ApplicationBase {
         return databaseService;
     }
 
-    public ClientConnectionManager gClientConnectionManager(){
-        return clientConnectionManager;
+    public ClientConnectionManager getConnectionManager(){
+        return ConnectionManager;
+    }
+
+    public MusicBandGetter gMusicBandGetter(){
+        return musicBandGetter;
     }
 
     public void useMusicBandManager(MusicBandManager musicBandManager) {
--- src/main/java/com/mfcms/laba/client/ConsoleApplication.java
+++ src/main/java/com/mfcms/laba/client/ConsoleApplication.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
+import com.mfcms.laba.client.console.ConsoleInterface;
 import com.mfcms.laba.client.net.ClientConnectionManager;
 import com.mfcms.laba.client.parsing.InteractiveFieldReader;
 import com.mfcms.laba.client.parsing.object.CoordinatesReader;
@@ -12,10 +13,10 @@ import com.mfcms.laba.client.parsing.object.MusicBandReader;
 import com.mfcms.laba.client.parsing.object.MusicGenreReader;
 import com.mfcms.laba.common.IOManager;
 import com.mfcms.laba.common.commands.*;
-import com.mfcms.laba.common.console.ConsoleInterface;
+import com.mfcms.laba.common.model.MusicBandGetter;
+import com.mfcms.laba.common.model.MusicBandManager;
 import com.mfcms.laba.common.net.ConnectionManager;
 import com.mfcms.laba.server.CommandService;
-import com.mfcms.laba.server.MusicBandManager;
 import com.mfcms.laba.server.database.DatabaseService;
 
 public class ConsoleApplication extends ApplicationBase {
@@ -35,21 +36,23 @@ public class ConsoleApplication extends ApplicationBase {
                 new MusicGenreReader(fieldReader));
 
         commandService = new CommandService();
-        clientConnectionManager = new ClientConnectionManager();
+        ConnectionManager = new ClientConnectionManager(null);
+        MusicBandGetter = new MusicBandGetter() ;
+
         commandExecutor = new ConsoleInterface(commandService, ioManager);
         databaseService = new DatabaseService("file.xml", musicBandManager);
 
         commandService.addCommands(
                 new HelpCommand(commandService, ioManager),
                 new ExitCommand(commandExecutor),
-                new AddCommand(clientConnectionManager, musicBandManager, musicBandReader),
+                new AddCommand(ConnectionManager, musicBandManager, musicBandReader),
                 new ShowCommand(musicBandManager, ioManager),
                 new SaveCommand(databaseService),
                 new UpdateCommand(musicBandManager, musicBandReader),
                 new RemoveCommand(musicBandManager),
-                new ClearCommand(clientConnectionManager, musicBandManager),
+                new ClearCommand(musicBandManager),
                 new RemoveByGenreCommand(musicBandManager),
-                new CountLessThanLabelCommand(clientConnectionManager, musicBandManager, ioManager),
+                new CountLessThanLabelCommand(musicBandManager, ioManager),
                 new ExecuteScriptCommand(musicBandManager));
     }
 }
